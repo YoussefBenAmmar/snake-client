34056c82661826045694f2bce40a8c598d1a5b7e
YoussefBenAmmar <benammar.y@gmail.com>
2023-01-18 13:51:29 -0500

    hard coded movement

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '192.168.0.59',
     port: 50541,
+
   });
 
   // interpret incoming data as text
@@ -14,9 +15,14 @@ const connect = function () {
     console.log("Server says: ", data);
   });
 
-  conn.on("connect", () => {
-    conn.write("Name: BOO")
-  })
+  // conn.on("connect", () => {
+  //   conn.write(`Move: up`);
+
+  //   setInterval(() => {
+  //     conn.write('Move: left');
+  //   }, 5000);
+
+  // });
 
   return conn;
 };
@@ -24,4 +30,4 @@ const connect = function () {
 console.log("Connecting ...");
 connect();
 
-module.exports = connect
+module.exports = connect;
